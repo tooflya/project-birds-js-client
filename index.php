ab7f6c96c1b41ad53f4ad847a32da2109ac2d0f9
Igor Mats <igor.mats@yandex.ru>
2014-01-20 22:45:54 +0200

    Update header licenses.

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <?
 
 /**
- * @file index.php
- * @category Executable files
+ * Tooflya Inc. Development
  *
  * @author Igor Mats from Tooflya Inc.
- * @copyright (c) 2012 by Igor Mats
+ * @copyright (c) 2013 by Igor Mats
+ * http://www.tooflya.com/development/
  *
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License v3
- * @all rights granted under this License are granted for the term of
- * copyright on the Program, and are irrevocable provided the stated
- * conditions are met.  This License explicitly affirms your unlimited
- * permission to run the unmodified Program.  The output from running a
- * covered work is covered by this License only if the output, given its
- * content, constitutes a covered work.  This License acknowledges your
- * rights of fair use or other equivalent, as provided by copyright law.
+ * License: Attribution NonCommercial NoDerivatives 4.0 International
+ *
+ * Creative Commons Corporation (“Creative Commons”) is not a law firm and does
+ * not provide legal services or legal advice. Distribution of Creative Commons
+ * public licenses does not create a lawyer-client or other relationship.
+ * Creative Commons makes its licenses and related information available on
+ * an “as-is” basis. Creative Commons gives no warranties regarding its licenses,
+ * any material licensed under their terms and conditions, or any related
+ * information. Creative Commons disclaims all liability for damages resulting
+ * from their use to the fullest extent possible.
+ *
+ * Creative Commons public licenses provide a standard set of terms and
+ * conditions that creators and other rights holders may use to share original
+ * works of authorship and other material subject to copyright and certain other
+ * rights specified in the public license below. The following considerations
+ * are for informational purposes only, are not exhaustive, and do not form part
+ * of our licenses.
+ *
+ * Creative Commons may be contacted at creativecommons.org.
+ *
+ * @version of cocos2d-x is 2.1.4
  *
  */
 
 require('../../config.php');
-
-/**
- *
- * Designed specifically for using this code in projects of Tooflya Inc.
- * Tooflya Inc., 2012
- *
- * @author Igor Mats from Tooflya Inc.
- * @copyright (c) 2012 by Igor Mats
- *
- */
