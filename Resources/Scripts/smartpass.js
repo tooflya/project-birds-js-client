1549a4e56a118b18dc75aded1919f6eacabb8922
Igor Mats <igor.mats@yandex.ru>
2014-05-26 17:10:28 +0300

    ?

--- Resources/Scripts/smartpass.js
+++ Resources/Scripts/smartpass.js
@@ -12,5 +12,37 @@ document.onreadystatechange = function() {
         });
       }, 1000);
     }
+
+    $(window).resize(function() {
+      var container = document.getElementById('canvas');
+      var canvas = $('div.canvas');
+
+      container.width = canvas.width();
+      container.height = canvas.height();
+      container.parentNode.style.width = canvas.width() + 'px';
+      container.parentNode.style.height = canvas.height() + 'px';
+
+      new Camera();
+    });
+
+    $(window).resize(function() {
+      resize();
+    });
+
+    $(window).on("orientationchange", function(e) {
+      resize();
+    });
   }
+}
+
+function resize() {
+  var container = document.getElementById('canvas');
+  var canvas = $('div.canvas');
+
+  container.width = canvas.width();
+  container.height = canvas.height();
+  container.parentNode.style.width = canvas.width() + 'px';
+  container.parentNode.style.height = canvas.height() + 'px';
+
+  new Camera();
 }
