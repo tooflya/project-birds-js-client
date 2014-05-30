e3d544c0d5bc3a07acaeeb52e463a20fe9a251ab
Igor Mats <igor.mats@yandex.ru>
2014-05-30 14:41:24 +0300

    Orientation changes experiments.

--- Resources/Scripts/smartpass.js
+++ Resources/Scripts/smartpass.js
@@ -12,46 +12,5 @@ document.onreadystatechange = function() {
         });
       }, 1000);
     }
-
-    $(window).resize(function() {
-      var container = document.getElementById('canvas');
-      var canvas = $('div.canvas');
-
-      container.width = canvas.width();
-      container.height = canvas.height();
-      container.parentNode.style.width = canvas.width() + 'px';
-      container.parentNode.style.height = canvas.height() + 'px';
-
-      new Camera();
-    });
-
-    $(window).resize(function() {
-      resize();
-    });
-
-    $(window).on("orientationchange", function(e) {
-      resize();
-    });
   }
 }
-
-function resize() {return;
-  var container = document.getElementById('canvas');
-  var canvas = $('div.canvas');
-
-  container.width = canvas.width();
-  container.height = canvas.height();
-  container.parentNode.style.width = canvas.width() + 'px';
-  container.parentNode.style.height = canvas.height() + 'px';
-
-  new Camera();
-}
-
-(function() {
-  var viewport = document.getElementById('viewport');
-  if(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
-    viewport.setAttribute("content", "width=device-width, maximum-scale=0.5, minimum-scale=0.5, initial-scale=0.5, user-scalable=no");
-  } else if(navigator.userAgent.match(/iPad/i)) {
-    viewport.setAttribute("content", "width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0, user-scalable=no");
-  }
-}(document));
